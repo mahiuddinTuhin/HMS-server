c75cbf37cbcabf5300033d16000eea343ccfa24a
mahiuddinTuhin <mahiuddiinntuhin@gmail.com>
2023-12-03 01:50:55 +0600

    dist included again

--- dist/middleware/globalErrorHandler.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.globalErrorHandler = void 0;
-const globalErrorHandler = (err, req, res, next) => {
-    const message = err.message || "Something went wring!";
-    res.status(400).json({ success: false, message, error: err });
-};
-exports.globalErrorHandler = globalErrorHandler;
