d950e3c6f0b9d8b90405682b74f7ecb8b2ad1ae6
mahiuddinTuhin <mahiuddiinntuhin@gmail.com>
2023-12-15 21:42:48 +0600

    department validation created

--- src/app/modules/department/departmentValidation.ts
+++ src/app/modules/department/departmentValidation.ts
@@ -1,5 +1,36 @@
 import { z } from "zod";
 
+/*
+ * medical problem validation with zod
+ */
+
+const medicalProblemValidation = z.object({
+  problemName: z.string(),
+
+  problemHints: z.string(),
+
+  symptoms: z.array(z.string()),
+
+  supportsFromHospital: z.array(z.string()),
+
+  treatments: z.array(z.string()),
+});
+
+/*
+ * medical specialization section validation with zod
+ */
+
+const specializationsValidation = z.object({
+  specializationName: z.string(),
+
+  specializationDetails: z.string(),
+
+  problems: z.array(medicalProblemValidation),
+});
+
+/*
+ * medical department section validation with zod
+ */
 const DepartmentValidation = z.object({
   departmentName: z
     .string({
@@ -7,14 +38,19 @@ const DepartmentValidation = z.object({
       invalid_type_error: "Department Name must be a string",
     })
     .min(3),
-  details: z
+
+  departmentDetails: z
     .string({
       required_error: "Detail of department is required",
       invalid_type_error: "Detail of department must be a string",
     })
     .min(20),
-  allDoctors: z.array(z.string().optional()).optional(),
-  licences: z.array(
+
+  specializations: z.array(specializationsValidation),
+
+  doctors: z.array(z.string().optional()).optional(),
+
+  medicalLicense: z.array(
     z
       .string({
         required_error: "Licences is required",
@@ -22,7 +58,14 @@ const DepartmentValidation = z.object({
       })
       .min(10),
   ),
-  allMedicalHistory: z.array(z.string().optional()).optional(),
+  medicalHistory: z.array(z.string().optional()).optional(),
+
+  contact: z.object({
+    email: z.array(z.string()),
+    phone: z.array(z.string()),
+    address: z.array(z.string()),
+  }),
+  isDeleted: z.boolean().default(false),
 });
 
 export default DepartmentValidation;
