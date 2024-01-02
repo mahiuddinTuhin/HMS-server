dd57e7eb97229f1daba9d22b7c2f18b156a59bde
mahiuddinTuhin <mahiuddiinntuhin@gmail.com>
2024-01-02 11:42:41 +0600

    medicalHistoy interface refact

--- src/app/modules/MedicalHistory/medicalHistory.ineterface.ts
+++ src/app/modules/MedicalHistory/medicalHistory.ineterface.ts
@@ -1,13 +1,19 @@
 import { Types } from "mongoose";
 
+type TMedications = {
+  date: Date;
+  medications: string[];
+};
+
 export type TMedicalHistory = {
   id: string;
   doctor: Types.ObjectId;
   patient: Types.ObjectId;
   diagonosis: Types.ObjectId[];
-  medications: string[];
+  medications: TMedications[];
   releasedOn?: string;
   bill?: number;
+  isPaid: boolean;
   doctorComments: string;
   patientComments: string;
   isDeleted: boolean;
